8e0f47c6b8efb370421ed993754572007e3d3724
BrunoNC22 <112676616+BrunoNC22@users.noreply.github.com>
2023-02-10 16:47:18 -0300

    Add files via upload

---
+++ SysLoc/src/main/java/com/sistemadelocacao/build/controller/ProdutoController.java
@@ -0,0 +1,37 @@
+package com.sistemadelocacao.build.controller;
+
+
+
+import com.sistemadelocacao.build.entities.Produto;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@RestController
+@RequestMapping("/apirest/produto")
+public class ProdutoController {
+	
+	@PostMapping("")
+	@ResponseStatus(HttpStatus.CREATED)
+	public void criar(
+			@RequestParam("id") int id,
+			@RequestParam("descricao") String descricao,
+			@RequestParam("valorDia") float valorDia,
+			@RequestParam("valorSemana") float valorSemana,
+			@RequestParam("valorMes") float valorMes
+			) {
+		Produto produto = new Produto(id, descricao, valorDia, valorSemana, valorMes);
+	}
+	
+	@GetMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public String recuperar (@PathVariable("id") int id) {
+		return "OK";
+	}
+}
