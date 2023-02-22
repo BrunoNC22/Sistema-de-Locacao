6c265c617dc6ff1a8abaa414ec3aa3853b56da63
BrunoNC22 <112676616+BrunoNC22@users.noreply.github.com>
2023-02-22 15:35:03 -0300

    Delete SysLoc directory

--- SysLoc/src/main/java/com/sistemadelocacao/build/repository/Boletos.java
+++
@@ -1,9 +0,0 @@
-package com.sistemadelocacao.build.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sistemadelocacao.build.entities.Boleto;
-
-public interface Boletos  extends JpaRepository<Boleto, Integer>{
-
-}
